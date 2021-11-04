0e30501d7d1e3beb35003c048649679664870d55
Madeline <42724159+Sirmentio@users.noreply.github.com>
2021-11-04 14:57:14 -0400

    Check for if DoDaylightCycle is enabled

--- src/main/java/im/hunnybon/ihgm/Ihgm.java
+++ src/main/java/im/hunnybon/ihgm/Ihgm.java
@@ -2,13 +2,19 @@ package im.hunnybon.ihgm;
 
 import net.fabricmc.api.ModInitializer;
 import net.fabricmc.fabric.api.event.lifecycle.v1.ServerTickEvents;
+import net.minecraft.world.GameRules;
 
 public class Ihgm implements ModInitializer {
     @Override
     public void onInitialize() {
         ServerTickEvents.START_SERVER_TICK.register((server) -> {
+            // Check first for if the player count is equal to exactly 0.
+            // If it is, them also check to see if the gamerule DoDaylightCycle
+            // is set to true, if it is, decrement the time of day by 1 each server tick.
             if (server.getCurrentPlayerCount() == 0){
-                server.getOverworld().setTimeOfDay(server.getOverworld().getTimeOfDay() - 1);
+                if (server.getGameRules().getBoolean(GameRules.DO_DAYLIGHT_CYCLE)){
+                    server.getOverworld().setTimeOfDay(server.getOverworld().getTimeOfDay() - 1);
+                }
             };
         });
     }
